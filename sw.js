c260a335fa9a1700b09c9d22a81095f9a6e80666
Arsen834-dev <Arsen834@bk.ru>
2026-06-20 13:48:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('install',function(e){e.waitUntil(caches.open('v1').then(function(c){return c.addAll(['/syndicate/','/syndicate/index.html'])}))});self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))})
+self.addEventListener('install',function(e){self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request))});
