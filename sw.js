dec5b8add715ff81bf13a311846d8d8b528d64fa
Arsen834-dev <Arsen834@bk.ru>
2026-06-20 11:01:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',function(e){e.waitUntil(caches.open('v1').then(function(c){return c.addAll(['/','/index.html'])}))});self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))})
+self.addEventListener('install',function(e){e.waitUntil(caches.open('v1').then(function(c){return c.addAll(['/syndicate/','/syndicate/index.html'])}))});self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))})
